befe42b56424526a13ab66a8bd47e8d56ccdfd8e
Wyatt Focht <wyatt.focht@duke.edu>
2020-11-30 23:56:15 -0500

    Finished content for events tab.  Could probably use more artistic eye

--- esgwebsite/src/Components/Tabs/Events/EBall.js
+++ esgwebsite/src/Components/Tabs/Events/EBall.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export default class EBall extends Component {
+    CONTENT = [
+        "E-Ball has been CANCELLED due to the effects of COVID-19.  ESG is currently working diligently to determine the most efficient and effective way to refund the cost of E-Ball tickets to those who have paid."
+    ]
 
+    CONTENT2 = [
+        "More information will be provided as the situation develops.  We appreciate your understanding in this difficult time."
+    ]
     render(){
         return(
-            <div>ball</div>
+            <div>
+                <h2>{this.CONTENT}</h2>
+                <h3>{this.CONTENT2}</h3>
+            </div>
         )
     }
 }
--- esgwebsite/src/Components/Tabs/Events/EPicnics.js
+++ esgwebsite/src/Components/Tabs/Events/EPicnics.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import {Grid, GridColumn, GridRow, Image } from 'semantic-ui-react'
 
 export default class EPicnics extends Component {
+    IMAGE_PATH = './images/';
+    DESCRIPTION = [
+        'We host E-Picnics once per semester. Come for music, free beer (for upperclassmen), and free food (for everyone). This event is also when we hand out our famous E-Picnic t-shirts that make all the Trinity students jealous.'
+    ]
 
     render(){
         return(
-            <div>picnic</div>
+            <div className='ePicnic'>
+                <Grid>
+                    <GridRow>
+                        <div>{this.DESCRIPTION}</div>
+                    </GridRow>
+                    <GridRow>
+                        <GridColumn width={16}>
+                             <Image circular src={require(this.IMAGE_PATH+'line-for-food.jpeg')}/>
+                        </GridColumn>
+
+                    </GridRow>
+                </Grid>
+            </div>
         )
     }
 }
