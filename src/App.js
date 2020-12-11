44fddefa0539ece56e72fda926d3a4a924d66b8b
Wyatt Focht <wyatt.focht@duke.edu>
2020-12-10 19:28:53 -0500

    Refactored routes into AppRoutes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import './App.css';
 import Footer from './Components/Footer.js'
-import StudentResources from './Components/Tabs/Student Resources/StudentResources.js'
-import Home from './Components/Tabs/Home/Home.js'
-import Contact from './Components/Tabs/Contact/Contact.js'
-import NavigationBar from './Components/NavigationBar.js'
-import About from './Components/Tabs/About/About.js';
-import Event from './Components/Tabs/Events/Events.js'
 import Animation from './Components/Animation.js'
-import {Route } from 'react-router-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Grid, GridRow, GridColumn} from 'semantic-ui-react';
 import { WindowContext } from './Components/WindowContext';
+import NavigationBar from './Components/NavigationBar.js'
+import AppRoutes from './Components/AppRoutes.js'
 
 class App extends React.Component {
 
-  // template: {path: , comp}
-ROUTES = [
-  {path: '/', comp: <Home/>},
-  {path: '/about', comp: <About/>},
-  {path: '/events', comp: <Event/>},
-  {path: '/contact', comp: <Contact/>},
-  {path: '/studentResources', comp: <StudentResources/>},
-]
 
 state = { width: 0, height: 0 };
 
@@ -39,25 +26,6 @@ state = { width: 0, height: 0 };
       this.setState({ width: window.innerWidth, height: window.innerHeight });
     };
 
-  createRoutes(){
-    var routes = []
-    for (var i = 0; i < this.ROUTES.length; i++){
-        routes[i] = this.createIndividualRoute(this.ROUTES[i]);
-    }
-    return routes;
-  }
-
-  createIndividualRoute(route){
-    return (
-      <Route
-      path={route.path}
-      exact
-      render={() => 
-      <div>{route.comp}
-      </div>}
-    />
-    )
-  }
 
   render(){
     return (
@@ -81,7 +49,7 @@ state = { width: 0, height: 0 };
       
           <GridRow>
             <div className='row'>
-              {this.createRoutes()}
+              <AppRoutes/>
           </div>
         </GridRow>
           
---
+++ src/Components/AppRoutes.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import StudentResources from './Tabs/Student Resources/StudentResources.js'
+import Home from './Tabs/Home/Home.js'
+import Contact from './Tabs/Contact/Contact.js'
+import About from './Tabs/About/About.js';
+import Event from './Tabs/Events/Events.js'
+import {Route} from 'react-router-dom'
+
+
+export default function AppRoutes() {
+
+    // template: {path: , comp}
+    const ROUTES = [
+    {path: '/', comp: <Home/>},
+    {path: '/about', comp: <About/>},
+    {path: '/events', comp: <Event/>},
+    {path: '/contact', comp: <Contact/>},
+    {path: '/studentResources', comp: <StudentResources/>},
+  ]
+
+  function createRoutes(){
+    var routes = []
+    for (var i = 0; i < ROUTES.length; i++){
+        routes[i] = createIndividualRoute(ROUTES[i]);
+    }
+    return routes;
+  }
+
+  function createIndividualRoute(route){
+    return (
+      <Route
+      path={route.path}
+      exact
+      render={() => 
+      <div>{route.comp}
+      </div>}
+    />
+    )
+  }
+
+
+        return(
+            <div>
+                {createRoutes()}
+            </div>
+        )
+
+
+}
