eebee53c4939a4adee9f6d2c901cb40d7afebb5f
Neil Upreti <neilxupreti@gmail.com>
2022-02-27 18:31:50 -0500

    small change on exec

--- src/Components/Tabs/About/ExecutiveBoard.js
+++ src/Components/Tabs/About/ExecutiveBoard.js
@@ -16,7 +16,7 @@ execBoardCards = [];
     {"name":"Jack Rhodes","graduation":2024,"title":"Vice President","email":"jsr73@duke.edu","fact":"Was secretary before this #promotion #movingupintheworld #hashtag","image":"JACKRHODES2.png"},
     {"name":"Ben Perry","graduation":2024,"title":"Chief Financial Officer","email":"benjamin.perry@duke.edu","fact":"Thought he was a senior all of last semester", "image":"benperry.png"},
     {"name":"Emily Shao","graduation":2024,"title":"Freshman Representative","email":"emily.shao@duke.edu","fact":"Told me to take ESG more seriously", "image":"emilyshao.png"},
-    {"name":"Long Nguyen","graduation":2025,"title":"Student Group Liason","email":"long.t.nguyen@duke.edu","fact":"Cool name, cooler person; I also like his crewneck in this picture","image":"longnguyen.png"},
+    {"name":"Long Nguyen","graduation":2025,"title":"Student Group Liason","email":"long.t.nguyen@duke.edu","fact":"I like his crewneck in this picture","image":"longnguyen.png"},
     //{"name":"Wyatt Focht","graduation":2023,"title":"Director of Technology and Communications","email":"wgf6@duke.edu","fact":"GREAT at coming up with fun facts","image":"wyattfocht.png"},
     {"name":"Aneesh Karuppur","graduation":2025,"title":"Director of Industry Relations","email":"aneesh.karuppur@duke.edu","fact":"Did not send me a picture O_O","image":"AneeshKaruppur.png"},
     {"name":"Sophia Leeman","graduation":2024,"title":"Chief Marketing Officer","email":"sophia.leeman@duke.edu","fact":"Friends with Ben","image":"leeman.png"},
